c4f344578769a0bbe2f92d961c50506541f82466
Mike LaGattuta <mjlagattuta@gmail.com>
2018-11-29 15:30:37 -0500

    Added build scripts for static files

---
+++ sources/tools/build-static/build-static.sh
@@ -0,0 +1,45 @@
+# Builds static fonts
+
+mkdir statics
+
+cp ExpletusSans.glyphs Build-Roman.glyphs
+cp ExpletusSans.glyphs Build-Italic.glyphs
+
+python2 $(dirname ${BASH_SOURCE[0]})/prep-roman.py Build-Roman.glyphs
+python2 $(dirname ${BASH_SOURCE[0]})/prep-italic.py Build-Italic.glyphs
+
+# —————————————————————————————————————————————————————————————————————————————————————————————————————————————
+# Build Roman —————————————————————————————————————————————————————————————————————————————————————————————————
+
+fontmake -o ttf -g Build-Roman.glyphs -i
+
+rm -rf master_ufo
+rm -rf instance_ufo
+
+for path in instance_ttf/*.ttf; do
+	filename=${path##*/}
+	ttfautohint $path statics/$filename -I
+done
+
+rm -rf instance_ttf
+
+rm -rf Build-Roman.glyphs
+
+
+# —————————————————————————————————————————————————————————————————————————————————————————————————————————————
+# Build Italic ————————————————————————————————————————————————————————————————————————————————————————————————
+
+fontmake -o ttf -g Build-Italic.glyphs -i
+
+rm -rf master_ufo
+rm -rf instance_ufo
+
+for path in instance_ttf/*.ttf; do
+	filename=${path##*/}
+	ttfautohint $path statics/$filename -I
+done
+
+rm -rf instance_ttf
+
+rm -rf Build-Italic.glyphs
+
