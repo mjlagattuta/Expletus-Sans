f6edd449f2387e1015a425b5c01d6ae06dc1a238
Mike LaGattuta <mjlagattuta@gmail.com>
2019-01-27 12:47:17 -0500

    Update linked VF build

--- sources/tools/build-linked/build-linked.sh
+++ sources/tools/build-linked/build-linked.sh
@@ -1,4 +1,4 @@
-# Builds weight axis only VFs ExpletusSans and ExpletusSansItalic linked via the STAT table
+# Builds weight axis only VFs ExpletusSans and ExpletusSans-Italic linked via the STAT table
 
 mkdir VF-linked
 
@@ -42,7 +42,7 @@ ttx ExpletusSans-VF.ttx
 
 rm -rf ExpletusSans-VF.ttx
 
-mv ExpletusSans-VF.ttf VF-linked/ExpletusSans-VF.ttf
+mv ExpletusSans-VF.ttf VF-linked/ExpletusSans-Roman-VF.ttf
 
 
 # —————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -54,31 +54,33 @@ rm -rf Build-Italic.glyphs
 
 cd variable_ttf
 
-mv ExpletusSans-ItalicItalic-VF.ttf ../ExpletusSansItalic-VF.ttf
+mv ExpletusSans-Italic-VF.ttf ../ExpletusSans-Italic-VF.ttf
 
 cd ..
 
 rm -rf master_ufo
 rm -rf variable_ttf
 
-gftools fix-nonhinting ExpletusSansItalic-VF.ttf ExpletusSansItalic-VF.ttf
-gftools fix-dsig --autofix ExpletusSansItalic-VF.ttf
-gftools fix-gasp ExpletusSansItalic-VF.ttf
+gftools fix-nonhinting ExpletusSans-Italic-VF.ttf ExpletusSans-Italic-VF.ttf
+gftools fix-dsig --autofix ExpletusSans-Italic-VF.ttf
+gftools fix-gasp ExpletusSans-Italic-VF.ttf
 
-ttx ExpletusSansItalic-VF.ttf
+ttx ExpletusSans-Italic-VF.ttf
 
-rm -rf ExpletusSansItalic-VF.ttf
-rm -rf ExpletusSansItalic-VF-backup-fonttools-prep-gasp.ttf
+rm -rf ExpletusSans-Italic-VF.ttf
+rm -rf ExpletusSans-Italic-VF-backup-fonttools-prep-gasp.ttf
 
-ttx ExpletusSansItalic-VF.ttf
 
-rm -rf ExpletusSansItalic-VF.ttf
-rm -rf ExpletusSansItalic-VF-backup-fonttools-prep-gasp.ttf
+cat ExpletusSans-Italic-VF.ttx | tr '\n' '\r' | sed -e "s~<STAT>.*<\/STAT>~$(cat $(dirname ${BASH_SOURCE[0]})/patchItalic-STAT.xml | tr '\n' '\r')~" | tr '\r' '\n' > ExpletusSans-Italic-VF2.ttx
 
-cat ExpletusSansItalic-VF.ttx | tr '\n' '\r' | sed -e "s~<name>.*<\/name>~$(cat $(dirname ${BASH_SOURCE[0]})/patchItalic-STAT.xml | tr '\n' '\r')~" | tr '\r' '\n' > VF-linked/ExpletusSansItalic-VF.ttx
+rm -rf ExpletusSans-Italic-VF.ttx
 
-rm -rf ExpletusSansItalic-VF.ttx
+mv ExpletusSans-Italic-VF2.ttx ExpletusSans-Italic-VF.ttx
 
-ttx VF-linked/ExpletusSansItalic-VF.ttx
+ttx ExpletusSans-Italic-VF.ttx
 
-rm -rf VF-linked/ExpletusSansItalic-VF.ttx
+rm -rf ExpletusSans-Italic-VF.ttx
+
+mv ExpletusSans-Italic-VF.ttf VF-linked/ExpletusSans-Italic-VF.ttf
+
+rm -rf instance_ufo
