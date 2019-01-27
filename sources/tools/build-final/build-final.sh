97d3ad8879af4f79bcab1f72f48196cc7ffc1244
Mike LaGattuta <mjlagattuta@gmail.com>
2019-01-27 12:46:29 -0500

    Update build for final VF

--- sources/tools/build-final/build-final.sh
+++ sources/tools/build-final/build-final.sh
@@ -1,3 +1,5 @@
+mkdir VF-final
+
 cp ExpletusSans.glyphs ExpletusSansBuild.glyphs
 
 # Add bracket layers to build version
@@ -11,7 +13,9 @@ rm -rf ExpletusSansBuild.glyphs
 rm -rf master_ufo
 rm -rf variable_ttf
 
-python2 $(dirname ${BASH_SOURCE[0]})/addItalics.py ExpletusSans-VF.ttf
+python2 addFeatureVars.py ExpletusSans-VF.ttf
+
+rm -rf addFeatureVars.py
 
 gftools fix-nonhinting ExpletusSans-VF.ttf ExpletusSans-VF.ttf
 gftools fix-dsig --autofix ExpletusSans-VF.ttf
@@ -22,12 +26,16 @@ ttx ExpletusSans-VF.ttf
 rm -rf ExpletusSans-VF.ttf
 rm -rf ExpletusSans-VF-backup-fonttools-prep-gasp.ttf
 
-cat ExpletusSans-VF.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/STAT>,$(cat $(dirname ${BASH_SOURCE[0]})/patch1.xml | tr '\n' '\r')," | tr '\r' '\n' > ExpletusSans-VF-STAT.ttx
+cat ExpletusSans-VF.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/STAT>,$(cat $(dirname ${BASH_SOURCE[0]})/patch-STAT.xml | tr '\n' '\r')," | tr '\r' '\n' > ExpletusSans-VF-STAT.ttx
 
 rm -rf ExpletusSans-VF.ttx
 
-ttx ExpletusSans-VF-STAT.ttx
+mv ExpletusSans-VF-STAT.ttx ExpletusSans-VF.ttx
 
-rm -rf ExpletusSans-VF-STAT.ttx
+ttx ExpletusSans-VF.ttx
 
-mv ExpletusSans-VF-STAT.ttf ExpletusSansBeta-VF.ttf
+mv ExpletusSans-VF.ttf VF-final/ExpletusSans-VF.ttf
+
+rm -rf ExpletusSans-VF.ttx
+
+rm -rf instance_ufo
